965de2d50be3d9e1ec9801f55b7ad2433443a296
Francesca Paoletti <francescagraham2000@yahoo.com>
2015-10-28 19:04:45 -0700

    Homepage Recipe routed

--- server.py
+++ server.py
@@ -4,8 +4,8 @@ from flask import Flask, render_template, redirect, request, flash, session
 
 from flask_debugtoolbar import DebugToolbarExtension
 
-# from recipe import (Recipe, User, Ingredient, RecipeStep, Category, RecipeUser,
-                    # RecipeIngredient,  connect_to_db, db)
+from model import (Recipe, User, Ingredient, RecipeStep, Category, RecipeUser,
+                    RecipeIngredient,  connect_to_db, db)
 
 
 app = Flask(__name__)
@@ -25,79 +25,95 @@ def index():
     return render_template("homepage.html")
 
 @app.route("/recipes")
-def user_list():
+def recipe_list():
     """Show list of users."""
 
-    u = session['user']
-
     # If user is logged in, search user's recipes.
-    if u:
-        recipes = u.Recipe.query.all()
-
+    # if session['user']:
+    # recipes = u.Recipe.query.all()
+    
     # If user is not logged in, return all recipes
-    else:
-        recipes = Recipe.query.all()
-        
+    # else:
+    recipes = Recipe.query.all()
+    print recipes
     return render_template("recipe_list.html", recipes=recipes)
 
-##############################################################################
-# REGISTER - LOGIN - LOGOUT 
-##############################################################################
-@app.route("/register")
-def register_user():
-    """Allows the user to sign up for an account"""
+# @app.route("/plan")
+# def plan():
+#     return render_template("planning.html")
 
-    return render_template("signup_form.html")
 
-@app.route("/register-confirm", methods=["POST"])
-def confirm_new_user():
-    """Create new user"""
+# @app.route("/grocery")   
 
-    user_email = request.form.get("email")
-    user_password = request.form.get("password") 
+# ##############################################################################
+# # REGISTER - LOGIN - LOGOUT 
+# ##############################################################################
+# @app.route("/register")
+# def register_user():
+#     """Allows the user to sign up for an account"""
+
+#     return render_template("signup_form.html")
+
+# @app.route("/register-confirm", methods=["POST"])
+# def confirm_new_user():
+#     """Create new user"""
+
+#     user_email = request.form.get("email")
+#     user_password = request.form.get("password") 
     
-    confirmed_user = User.get_user_by_email(user_email)
+#     confirmed_user = User.get_user_by_email(user_email)
     
-    if not confirmed_user:
-        User.create_user_by_email_password(user_email, user_password)
-        flash("You successfully created an account!")
-    else:
-        flash("You already have an account")
+#     if not confirmed_user:
+#         User.create_user_by_email_password(user_email, user_password)
+#         flash("You successfully created an account!")
+#     else:
+#         flash("You already have an account")
 
-    return redirect("/")
+#     return redirect("/")
 
-@app.route("/login")
-def login_user():
-    """Logs the user in"""
+# @app.route("/login")
+# def login_user():
+#     """Logs the user in"""
 
-    return render_template("login_form.html")
+#     return render_template("login_form.html")
 
-@app.route("/logout")
-def logout_user():
-    """Logs out the user"""
-    del session['User']
+# @app.route("/logout")
+# def logout_user():
+#     """Logs out the user"""
+#     del session['User']
     
-    flash("You are logged out","loggedout")
+#     flash("You are logged out","loggedout")
 
-    return redirect("/")    
+#     return redirect("/")    
 
 
-@app.route('/login_confirm', methods=["POST"])
-def get_login():
-    """Get user info"""
-    user_email = request.form.get("email")
-    user_password = request.form.get("password")
+# @app.route('/login_confirm', methods=["POST"])
+# def get_login():
+#     """Get user info"""
+#     user_email = request.form.get("email")
+#     user_password = request.form.get("password")
     
-    confirmed_user = User.get_user_by_email_password(user_email, user_password)
+#     confirmed_user = User.get_user_by_email_password(user_email, user_password)
     
-    if confirmed_user:
-        flash("You're logged in!","loggedin")
-        userid = confirmed_user.user_id
-        session["User"] = userid
+#     if confirmed_user:
+#         flash("You're logged in!","loggedin")
+#         userid = confirmed_user.user_id
+#         session["User"] = userid
 
-        print session["User"]
-        return redirect("/")
-    else:
-        flash("Your email and password combination are not correct.")
-        return redirect("/login")
+#         print session["User"]
+#         return redirect("/")
+#     else:
+#         flash("Your email and password combination are not correct.")
+#         return redirect("/login")
 
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the point
+    # that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run()
